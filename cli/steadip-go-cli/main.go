b5b2bbeef65177f5ca8fc61f8a48f48f96318250
Maxime Labelle <mlabelle@ip4b.ca>
2026-08-01 10:48:54 -0400

    .

--- cli/steadip-go-cli/main.go
+++ cli/steadip-go-cli/main.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	version         = "0.2.1"
+	version         = "0.2.2"
 	frpVersion      = "0.70.1"
 	apiBase         = "https://steadip.com/api"
 	versionBase     = "https://raw.githubusercontent.com/mlowasp/steadip/main/cli/steadip-go-cli/dist/current_version"
